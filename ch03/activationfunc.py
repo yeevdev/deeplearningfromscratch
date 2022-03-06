a215219359e791e516ce0780a7a25cbab2aff3f3
yeevdev <hufskhj1@gmail.com>
2022-03-06 22:56:11 +0900

    deeplearning

--- ch03/activationfunc.py
+++ ch03/activationfunc.py
@@ -6,8 +6,8 @@ def step_function(x):
     y = x > 0
     return y.astype(np.int)
 
-x = np.arange(-5.0, 5.0, 0.1)
-y = step_function(x)
-plt.plot(x, y)
-plt.ylim(-0.1, 1.1)
-plt.show()
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def relu(x):
+    return np.maximum(0, x)
